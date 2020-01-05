a12f054c51f2c485c8074310855699177df3ea0f
root <root@ubuntu-18.04-desktop-lts-amd64>
2020-01-05 12:23:25 +0100

    Avanzando en los test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,3 +1,5 @@
+const app = require('../server.js');
+const request = require('supertest');
 
 describe("Hello world tests", () => {
     it("Should do an stupid test", () => {
@@ -6,5 +8,17 @@ describe("Hello world tests", () => {
         const sum = a + b;
 
         expect(sum).toBe(8);
+    });
+});
+
+describe("Carnets API", () => {
+    describe("GET /", () => {
+        it("Should return an HTML document", () => {
+            request(app).get("/").then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.type).toEquals(expect.stringContains("html"));
+                expect(response.text).toEquals(expect.stringContains("h1"));
+            });
+        });
     });
 });

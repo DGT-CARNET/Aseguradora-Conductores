e22f8a39eba861c0027f4d35ae0d5f350f986d87
root <root@ubuntu-18.04-desktop-lts-amd64>
2020-01-14 19:37:03 +0100

    Actualmente consigue autorizar

--- passport.js
+++ passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const LocalAPIKey = require('passport-localapikey-update').Strategy
+const ApiKey = require('./apikeys');
 
 passport.use(new LocalAPIKey(
     (apikey,done) => {
